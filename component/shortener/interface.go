fdeb03566a7d4606cdc5625f487159c776c0f73a
Ekkasith Wisanphokha <ekkasithw@Ekkasiths-MacBook-Pro.local>
2021-08-13 23:03:48 +0700

    0.9.3-ComponentDiagram

--- component/shortener/interface.go
+++ component/shortener/interface.go
@@ -8,18 +8,18 @@ type Comp interface {
 	AccessURL(input *AccessInput) (output *ShortenOutput, err error)
 }
 
-//go:generate mockery --name=URLCache
-type URLCache interface {
-	Read(ID string) (ent *entity.URL)
-	Write(ent *entity.URL) (err error)
-}
-
 //go:generate mockery --name=URLRepo
 type URLRepo interface {
 	Create(ent *entity.URL) (err error)
 	Read(ID string) (ent *entity.URL, err error)
 	CountID(ID string) (count int, err error)
 	IncrHit(ID string) (err error)
+}
+
+//go:generate mockery --name=URLCache
+type URLCache interface {
+	Read(ID string) (ent *entity.URL)
+	Write(ent *entity.URL) (err error)
 }
 
 //go:generate mockery --name=Validator
